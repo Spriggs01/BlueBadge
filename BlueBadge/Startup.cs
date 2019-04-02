adb446de3516272f74f9221c5565750181fd9c3c
Spriggs01 <sarah.spriggs@gamil.com>
2019-04-02 19:39:51 -0400

    Add project files.

---
+++ BlueBadge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlueBadge.Startup))]
+namespace BlueBadge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
