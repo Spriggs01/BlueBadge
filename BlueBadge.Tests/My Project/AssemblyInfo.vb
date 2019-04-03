c3126021e851bccd485fb22ba8070ab707e84a99
Spriggs01 <sarah.spriggs@gamil.com>
2019-04-03 10:33:13 -0400

    first commit

---
+++ BlueBadge.Tests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("BlueBadge.Tests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("BlueBadge.Tests")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("5c025109-b980-4940-89ba-ad04d3f684cc")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
--- BlueBadge/App_Start/IdentityConfig.cs
+++ BlueBadge/App_Start/IdentityConfig.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using BlueBadge.Models;
+using BlueBadge.Data;
 
 namespace BlueBadge
 {
